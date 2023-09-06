f34651eab33df45d170c6f734d2bc9fcef9239af
Bhushan345566 <bhushanmandala45@gmail.com>
2023-09-06 10:19:13 +0530

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import Setpassword from './Setpassword';
 function App() {
   return (
     <div>
-      <h1>jhbibhushan</h1>
+    
     <Routes>
       <Route path='/Login' element={<Login/>}/>
       <Route path='/' element={<Signup/>}/>
